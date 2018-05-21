d54ce3278168f084268c18afbcfba8bad69d8aee
Anusha <anusha.mathew@st.ovgu.de>
2018-05-22 00:28:49 +0200

    Attempt

--- app/src/main/java/com/example/teamatom/symbiote/MainActivity2.java
+++ app/src/main/java/com/example/teamatom/symbiote/MainActivity2.java
@@ -1,13 +1,14 @@
 package com.example.teamatom.symbiote;
-
-import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
-public class MainActivity2 extends Activity {
 
+public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +16,68 @@ public class MainActivity2 extends Activity {
         setContentView(R.layout.activity_second);
 
 
-            }
 
+
+        LinearLayout first = (LinearLayout )findViewById(R.id.first);
+        first.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent j = new Intent(MainActivity2.this, enterAct.class);
+                startActivity(j);
+            }
+        });
+
+        LinearLayout second = (LinearLayout )findViewById(R.id.second);
+        second.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent j = new Intent(MainActivity2.this, enterAct.class);
+                startActivity(j);
+            }
+        });
+
+
+        LinearLayout third = (LinearLayout )findViewById(R.id.third);
+        third.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent j = new Intent(MainActivity2.this, enterAct.class);
+                startActivity(j);
+            }
+        });
+
+
+        LinearLayout fourth = (LinearLayout )findViewById(R.id.fourth);
+        fourth.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent j = new Intent(MainActivity2.this, reset_pin.class);
+                startActivity(j);
+            }
+        });
+
+
+        LinearLayout fifth = (LinearLayout )findViewById(R.id.fifth);
+        fifth.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent j = new Intent(MainActivity2.this, enterAct.class);
+                startActivity(j);
+            }
+        });
+
+
+        LinearLayout sixth = (LinearLayout )findViewById(R.id.sixth);
+        sixth.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent j = new Intent(MainActivity2.this, enterAct.class);
+                startActivity(j);
+            }
+        });
+
+
+
+
+    }
 }
